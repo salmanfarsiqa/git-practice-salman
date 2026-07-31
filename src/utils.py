91a2e70159b8bd53cf6aefd1003d1811e56ffad0
salmanfarsiqa <salmanalibiu@gmail.com>
2026-08-01 00:39:51 +0600

    Add error handling for calculator

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,28 @@
 def add(a, b):
-    return a + b
+    try:
+        return a + b
+    except TypeError:
+        return "Invalid inputs for add"
 
 
 def subtract(a, b):
-    return a - b
+    try:
+        return a - b
+    except TypeError:
+        return "Invalid inputs for subtract"
 
 
 def multiply(a, b):
-    return a * b
+    try:
+        return a * b
+    except TypeError:
+        return "Invalid inputs for multiply"
+
+
+def divide(a, b):
+    try:
+        if b == 0:
+            return "Cannot divide by zero"
+        return a / b
+    except TypeError:
+        return "Invalid inputs for divide"
